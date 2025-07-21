2635550ac6c65a3574ea38424e57d593285166f7
ommnyomnyong <kimiy990728@gmail.com>
2025-07-21 16:09:24 +0900

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
+import os
+
 st.title('배포 테스트')
 st.write('배포 테스트용 앱')
+
+st.write('추가 커밋')
+
+api_key = os.environ.get('API_SECRET_KEY')
+
+st.write(f'api key = {api_key}')
+
+if not os.environ.get('DB_HOST'):
+    st.write('DB 정보가 없음')
+else:
+    conn = pymysql.connect(host=os.environ.get('API_SECRET_KEY'))
